07ea4d3b1cfef4c40ac032fb468c1b0ea3ea3021
rajcsa <97621471+rajcsa@users.noreply.github.com>
2022-01-24 09:29:27 -0800

    owierwer

---
+++ Unit_04/src/DiscountRunner.java
@@ -0,0 +1,24 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - 
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class DiscountRunner
+{
+	public static void main( String args[] )
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		out.print("Enter the original bill amount :: ");
+		double amt = keyboard.nextDouble();
+		
+		double res = Discount.getDiscountedBill(amt);
+		System.out.println("Bill after discount: " + res);
+	}
+}
