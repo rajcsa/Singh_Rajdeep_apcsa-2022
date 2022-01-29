15ab71906d24f2455766bfaa66c319f702d4192e
rajcsa <97621471+rajcsa@users.noreply.github.com>
2022-01-28 23:54:29 -0800

    Update WordsCompareRunner.java

--- Unit_05/src/WordsCompareRunner.java
+++ Unit_05/src/WordsCompareRunner.java
@@ -12,7 +12,29 @@ public class WordsCompareRunner
 {
    public static void main( String args[] )
    {
-		//add test cases
+		WordsCompare test = new WordsCompare("abe","ape");
+		out.println(test);
+		
+		test.setWords("giraffe", "gorilla");
+		out.println(test);
+		
+		test.setWords("one", "two");
+		out.println(test);
+		
+		test.setWords("fun", "funny");
+		out.println(test);
+		
+		test.setWords("123", "19");
+		out.println(test);
+		
+		test.setWords("193", "1910");
+		out.println(test);
+		
+		test.setWords("goofy", "godfather");
+		out.println(test);
+		
+		test.setWords("funnel", "fun");
+		out.println(test);
 
 	}
 }
