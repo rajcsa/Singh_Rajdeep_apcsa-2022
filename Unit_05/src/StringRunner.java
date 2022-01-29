779ec8ab91328790bac0746e045f0372ebef9a8a
rajcsa <97621471+rajcsa@users.noreply.github.com>
2022-01-28 23:49:48 -0800

    Update StringRunner.java

--- Unit_05/src/StringRunner.java
+++ Unit_05/src/StringRunner.java
@@ -13,5 +13,20 @@ public class StringRunner
 {
 	public static void main ( String[] args )
 	{
+		StringOddOrEven obj = new StringOddOrEven();
+		int whileLoop=0;
+		Scanner keyboard = new  Scanner(System.in);
+		
+		while (whileLoop<=7){
+		out.print("Enter a word :: ");
+		
+		String strWord = keyboard.next(); 
+		
+			obj.setString(strWord);
+		out.println(obj);
+		
+		out.println();
+		whileLoop++;
+		 }				
 	}
 }
