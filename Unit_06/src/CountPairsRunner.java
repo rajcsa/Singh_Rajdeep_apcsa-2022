687a183d53ffdd441e9652bb2a8ab48a91f60968
rajcsa <97621471+rajcsa@users.noreply.github.com>
2022-02-02 23:50:51 -0800

    Update CountPairsRunner.java

--- Unit_06/src/CountPairsRunner.java
+++ Unit_06/src/CountPairsRunner.java
@@ -8,6 +8,11 @@ public class CountPairsRunner
 {
 	public static void main( String[] args )
 	{
+		
+		// N/A, CountPairs is not a class (errors on home computer)
+		
+		
+		
 		System.out.println( CountPairs.pairCounter("test_cases") );
 		//add in all of the provided test cases from the lab handout	
 	}
