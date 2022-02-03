836b7eee31bdc7cb47f96c442a9840cb4089b62a
rajcsa <97621471+rajcsa@users.noreply.github.com>
2022-02-02 23:26:36 -0800

    Update LetterRemoverRunner.java

--- Unit_06/src/LetterRemoverRunner.java
+++ Unit_06/src/LetterRemoverRunner.java
@@ -2,13 +2,24 @@
 //www.apluscompsci.com
 //Name -
 
-import static java.lang.System.*;
-
 public class LetterRemoverRunner
 {
 	public static void main( String args[] )
 	{
-		//add test cases		
+		// add test cases		
+		out.println("Lab Chapter 9 - #8  LetterRemover    2016");
+		out.println();
+		out.println();
+		
+		// ***** fill in your name
+		out.println("My name is ????????  ????????????????");
+		out.println();		
+		out.println();		
+		
+
+		
 											
-	}
-}
+	}  // end marker for method main
+	
+	
+} // end marker for class 
