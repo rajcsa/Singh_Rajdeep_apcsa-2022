27b7ee6938186a3bf3c1253fd2eabfbfeb9f19dd
rajcsa <97621471+rajcsa@users.noreply.github.com>
2022-02-02 23:27:57 -0800

    Update Perfect.java

--- Unit_06/src/Perfect.java
+++ Unit_06/src/Perfect.java
@@ -1,22 +1,57 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
-
 import static java.lang.System.*;
 
 public class Perfect
 {
    private int number;
 
-	//add constructors
+    //add constructors
+    public Perfect(){
+        number = 0;
+        this.isPerfect();
+    }
+    
+    public Perfect(int num){
+        this.setNums(num);
+        this.isPerfect();
+    }
+    //add a set method
+    
+    public void setNums(int num){
+        number = num;
+    }
 
-	//add a set method
+    public boolean isPerfect()
+    {
 
-	public boolean isPerfect()
-	{
-		return false;
-	}
+        int div = 0;
+        int sum = 0;
+        while(div<number/2){
+            div++;
+            if(number%div==0){
+                sum+=div;
+            }
+        }
+        if(sum==number){
+            System.out.println(number + " " + " is perfect.");
+            return true;
+        }
+        else{
+            System.out.println(number + " " + " is not perfect.");
+            return false;
+        }
+    }
 
-	//add a toString	
-	
+    //add a toString
+    public String toString(){
+        if(this.isPerfect()){
+            return number + " " + " is perfect.";
+        }
+        else{
+            return number + " " + " is not perfect.";
+        }
+    }
+    
 }
