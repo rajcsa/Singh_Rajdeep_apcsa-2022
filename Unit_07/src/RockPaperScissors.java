388a4853a7e96828187eda54a89c1914ec036fc4
rajcsa <97621471+rajcsa@users.noreply.github.com>
2022-02-07 22:59:05 -0800

    Update RockPaperScissors.java

--- Unit_07/src/RockPaperScissors.java
+++ Unit_07/src/RockPaperScissors.java
@@ -4,33 +4,118 @@
 
 import java.util.Scanner;
 import static java.lang.System.*;
+import java.awt.Window;
 
 public class RockPaperScissors
 {
-	private String playChoice;
-	private String compChoice;
+    private char playChoice;
+    private char compChoice;
 
-	public RockPaperScissors()
-	{
-	}
+    public RockPaperScissors()
+    {
 
-	public RockPaperScissors(String player)
-	{
-	}
+        playChoice='r';
+        compChoice='p';
 
-	public void setPlayers(String player)
-	{
-	}
+    }
 
-	public String determineWinner()
-	{
-		String winner="";
-		return winner;
-	}
+    public RockPaperScissors(char player)
+    {
+        playChoice = player;
+        this.setPlayers(playChoice);
+    }
 
-	public String toString()
-	{
-		String output="";
-		return output;
-	}
+    public void setPlayers(char player)
+    {
+
+
+        playChoice = player;
+        out.println("you chose " + playChoice);
+        double cc2 = 3*Math.random();
+        int cc = (int)cc2;
+        if (cc==0)
+            compChoice='r';
+        else if (cc==1)
+            compChoice='p';
+        else
+            compChoice='s';
+        this.determineWinner();
+    }
+
+    public String determineWinner()
+    {
+        String winner="";
+        
+        
+        //out.print("one " + playChoice);
+        if(playChoice==compChoice){
+            out.println("You tied! Play again?");
+            winner = "You tied! Play again?";
+            this.restart();
+        }
+
+        else if(playChoice == 'r'){
+            switch(compChoice){
+                case 'p': out.println("You lose! Paper covers rock."); winner = "You lose! Paper covers rock."; break;
+                case 's': out.println("You win! Rock breacks scissors."); winner = "You win! Rock breacks scissors."; break;
+                default: out.print("faillllllll");
+            }
+            this.restart();
+        }
+
+        else if(playChoice=='p'){
+            switch(compChoice){
+                case 'r': out.println("You win! Paper covers rock."); winner = "You win! Paper covers rock."; break;
+                case 's': out.println("You lose! Scissors cuts paper"); winner = "You lose! Scissors cuts paper."; break;
+            }
+            this.restart();
+        }
+
+        else if(playChoice=='s'){
+            switch(compChoice){
+                case 'r': out.println("You lose! Rock breaks scissors."); winner = "You lose! Rock breaks scissors."; break;
+                case 'p': out.println("You win! Scissors cut paper."); winner = "You win! Scissors cut paper."; break;
+            }
+            this.restart();
+        }
+        else {
+            out.println("fail");
+        }
+
+        //out.print(" two " + compChoice);
+
+        return winner;
+    }
+
+    public String toString()
+    {
+        return determineWinner();
+    }
+    
+    public void restart(){
+        Scanner k = new Scanner(System.in);
+        out.print("Would you like to play? [y, n] :: ");
+        char res = k.next().charAt(0);
+        if (res=='y'){
+            Scanner keyboard = new Scanner(System.in);
+            char response;
+        
+            //add in a do while loop after you get the basics up and running
+        
+            String play;
+        
+            out.print("Rock-Paper-Scissors - pick your weapon [r,p,s] :: ");
+            
+
+            //read in the player value
+            response = keyboard.nextLine().charAt(0);
+            //play = Character.toString(response);
+        
+            RockPaperScissors game = new RockPaperScissors(response);
+        }
+        else{
+            out.print("G O O D B Y E");
+            System.exit(0); 
+        }
+    }
 }
