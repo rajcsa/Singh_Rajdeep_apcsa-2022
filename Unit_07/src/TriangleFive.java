ce5aa0a575eb2e24eab81d52ae69a4ec0546ae3a
rajcsa <97621471+rajcsa@users.noreply.github.com>
2022-02-08 09:08:36 -0800

    .

--- Unit_07/src/TriangleFive.java
+++ Unit_07/src/TriangleFive.java
@@ -11,23 +11,46 @@ public class TriangleFive
 
 	public TriangleFive()
 	{
+		setLetter('#');
+		setAmount(1);
 	}
 
 	public TriangleFive(char c, int amt)
 	{
+		setLetter(c);
+		setAmount(amt);
 	}
 
 	public void setLetter(char c)
 	{
+		c = letter;
+		
+		
 	}
 
 	public void setAmount(int amt)
 	{
+		amt = amount;
 	}
 
 	public String toString()
 	{
 		String output="";
-		return output;
+		
+		for (int i = 0; i < amount; i++) {
+			for (int j = amount - 1; j >= i; j--) {
+				output += " ";
+			}
+			
+			for (int j = i - 1; j >= 0; j--) {
+				output += letter;
+			}
+			
+			output += '\n';
+			
+			
+		}
+		
+		return output + "\n";
 	}
 }
--- Unit_07/src/TriangleFiveRunner.java
+++ Unit_07/src/TriangleFiveRunner.java
@@ -8,5 +8,9 @@ public class TriangleFiveRunner
 {
    public static void main(String args[])
    {
+	   TriangleFive test = new TriangleFive();
+	   test.setLetter('C');
+	   test.setAmount(3);
+	   out.print(test);
 	}
 }
