b9724d24bcc5e8a17f4a44a5d36ade3b2ebf5bf1
rajcsa <97621471+rajcsa@users.noreply.github.com>
2022-02-10 09:16:22 -0800

    Update RayOddToEven.java

--- Unit_08/src/RayOddToEven.java
+++ Unit_08/src/RayOddToEven.java
@@ -5,8 +5,23 @@
 
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	public static int go(int[] ray)
 	{
-		return false;
+		int first_odd = 0;
+		int first_even = 0;
+		
+		for (int c : ray) {
+			if (c%2==1) {
+				first_odd = c;
+			}
+			if ((c%2==0) && (first_even == 0)) {
+				first_even = c;
+				break;
+			}
+			
+			
+			
+		}
+		return first_odd-first_even;
 	}
 }
