5144d15d89a44da2ae7ac3a0ce7890d6aa1db70d
rajcsa <97621471+rajcsa@users.noreply.github.com>
2022-02-10 22:32:46 -0800

    Update RayDown.java

--- Unit_08/src/RayDown.java
+++ Unit_08/src/RayDown.java
@@ -9,6 +9,12 @@ public class RayDown
 	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(int[] numArray)
 	{
-		return true;
+		for (int num = 0; (num < numArray.length && num+1< numArray.length); num ++)
+			if (numArray[num] > numArray[num+1])
+				return true;
+			else 
+				return false;
+		return false;
 	}	
+	
 }
