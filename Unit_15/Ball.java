29fa37b5023538f88e74f3e45f4bab9ea6cb0a1b
rajcsa <97621471+rajcsa@users.noreply.github.com>
2022-04-11 11:33:30 -0700

    Update Ball.java

--- Unit_15/Ball.java
+++ Unit_15/Ball.java
@@ -1,1 +1,102 @@
-// initial commit
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Ball extends Block
+{
+	private int xSpeed;
+	private int ySpeed;
+
+	public Ball()
+	{
+		super(200,200);
+		xSpeed = 3;
+		ySpeed = 1;
+	}
+
+	//add the other Ball constructors
+	
+	public Ball(int x, int y)
+	{
+		super(x, y);
+		xSpeed = 3;
+		ySpeed = 1;
+	}
+	
+	public Ball(int x, int y, int wid, int ht)
+	{
+		super(x, y, wid, ht);
+		xSpeed = 3;
+		xSpeed = 1;
+	}
+	
+	public Ball(int x, int y, int wid, int ht, Color col)
+	{
+		super(x, y, wid, ht, col);
+		xSpeed = 3;
+		xSpeed = 1;
+	}
+	
+	public Ball(int x, int y, int wid, int ht, int xSpd, int ySpd)
+	{
+		super(x, y, wid, ht);
+		xSpeed = xSpd;
+		xSpeed = ySpd;
+	}
+	
+	public Ball(int x, int y, int wid, int ht, Color col, int xSpd, int ySpd)
+	{
+		super(x, y, wid, ht, col);
+		xSpeed = xSpd;
+		xSpeed = ySpd;
+	}
+	
+   //add the set methods
+   public void setXSpeed(int xSpd)
+   {
+	   xSpeed = xSpd;
+   }
+   
+   public void setYSpeed(int ySpd)
+   {
+	   ySpeed = ySpd;
+   }
+
+   public void moveAndDraw(Graphics window)
+   {
+   	//draw a white ball at old ball location
+	   draw(window, Color.white);
+	   
+
+      setX(getX()+xSpeed);
+		//setY
+
+		//draw the ball at its new location
+   }
+   
+	public boolean equals(Object obj)
+	{
+		Ball other = (Ball)obj;
+		if (super.equals(other) && getXSpeed() == other.getXSpeed() && getYSpeed() == other.getYSpeed())
+		{
+			return true;
+		}
+		return false;
+	}   
+
+   //add the get methods
+	public int getXSpeed()
+	{
+		return xSpeed;
+	}
+	
+	public int getYSpeed()
+	{
+		return ySpeed;
+	}
+
+   //add a toString() method
+	public String toString()
+	{
+		return super.toString() + "xSpeed: " + xSpeed + "ySpeed: " + ySpeed;
+	}
+}
